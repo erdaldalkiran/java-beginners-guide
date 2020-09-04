a7b2159ddebf992824b8102fc0715a0a66bc93b9
Erdal Dalkiran <ed@erdaldalkiran.com>
2020-09-04 15:33:09 +0300

    ed adds ch3 sample code

---
+++ ch3/Input.java
@@ -0,0 +1,10 @@
+
+import java.io.IOException;
+
+
+ class Input {
+    public static void main(final String args[]) throws IOException {
+        final var line = System.in.read();
+        System.out.println((char)line);
+    }
+}
