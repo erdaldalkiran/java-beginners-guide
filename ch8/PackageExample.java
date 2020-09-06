0c85a02929091a3a49593b410d0c46d0017ce87b
Erdal Dalkiran <ed@erdaldalkiran.com>
2020-09-06 12:12:54 +0300

    ed adds package code sample

---
+++ ch8/PackageExample.java
@@ -0,0 +1,10 @@
+package ch8;
+
+class PackageExample{
+
+public static void main(String[] args){
+    System.out.println("to build and run at th current folder:");
+    System.out.println("javac PackageExample.java");
+    System.out.println("java -classpath ..  ch8.PackageExample ");
+}
+}
